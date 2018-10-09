419245476fdc6177b7a1901f054740cddccd9861
freewind <nowindlee@gmail.com>
2018-10-09 16:36:27 +0800

    rename: .html -> .ejs

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: 'template.html',
+            template: 'template.ejs',
             inject:false
         })
     ]
